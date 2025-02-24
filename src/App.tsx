9e174732d08c4460a7323e0726a4d014f4f580db
Cladamos <caliskangokberk@gmail.com>
2025-02-25 00:13:49 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button, MantineProvider } from "@mantine/core"
+
+function App() {
+  return (
+    <MantineProvider>
+      <Button>Merhaba</Button>
+    </MantineProvider>
+  )
+}
+
+export default App
