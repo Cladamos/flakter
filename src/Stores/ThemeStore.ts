b852d123dacd3b5fba1da6ad0f7435b36b7d3729
Cladamos <caliskangokberk@gmail.com>
2025-02-28 14:41:59 +0300

    fix: npm build issue

--- src/Stores/ThemeStore.ts
+++ src/Stores/ThemeStore.ts
@@ -10,7 +10,7 @@ interface ThemeStore {
 
 export const useThemeStore = create(
   persist<ThemeStore>(
-    (set, get) => ({
+    (set) => ({
       themeColor: "indigo",
       theme: createTheme({
         fontFamily: "Poppins, sans-serif",
