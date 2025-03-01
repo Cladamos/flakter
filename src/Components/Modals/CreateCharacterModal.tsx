7fe8fc05584dd0b5c69a2faf927b31c581864f2f
Cladamos <caliskangokberk@gmail.com>
2025-03-01 19:36:23 +0300

    chore: add translation to create character modal

--- src/Components/Modals/CreateCharacterModal.tsx
+++ src/Components/Modals/CreateCharacterModal.tsx
@@ -26,29 +26,7 @@ import { useThemeStore } from "../../Stores/ThemeStore"
 import { useMediaQuery } from "@mantine/hooks"
 import { IconCircleFilled, IconInfoCircle } from "@tabler/icons-react"
 import { modals } from "@mantine/modals"
-
-const basics = [
-  { label: "Name", placeholder: "Geoff Jones", key: "name" },
-  { label: "High Concept", placeholder: "Noir Style hard punches and hard liquor detective", key: "aspects.highConcept" },
-  { label: "Trouble", placeholder: "I can't run from my past life but I can watch him trying to catch me", key: "aspects.trouble" },
-  { label: "Relationship", placeholder: "Knows all the cool car shops", key: "aspects.relationship" },
-  { label: "Other Aspect", placeholder: "Liquor and revenge keep me going", key: "aspects.otherAspect" },
-  { label: "Other Aspect", placeholder: "I won't die without revealing the truth", key: "aspects.secondOtherAspect" },
-]
-
-const stunts = [
-  { label: "Stunt 1", key: "stunts.0" },
-  { label: "Stunt 2", key: "stunts.1" },
-  { label: "Stunt 3", key: "stunts.2" },
-  { label: "Stunt 4", key: "stunts.3" },
-  { label: "Stunt 5", key: "stunts.4" },
-  { label: "Stunt 6", key: "stunts.5" },
-]
-
-const stressBoxes = [
-  { label: "Physical stress", key: "maxPhysicalStress" },
-  { label: "Mental stress", key: "maxMentalStress" },
-]
+import { useTranslation } from "react-i18next"
 
 const colors = ["gray", "red", "pink", "grape", "violet", "indigo", "blue", "cyan", "teal", "green", "lime", "yellow", "orange"] // Comes here https://yeun.github.io/open-color/
 
@@ -66,6 +44,31 @@ function CreateCharacterModal(props: createCharacterModalProps) {
   const isMobile = useMediaQuery(`(max-width: ${em(1200)})`)
   const { themeColor, setThemeColor } = useThemeStore()
   const { currCharacter, addCharacter, updateCharacter } = useCharacterStore()
+
+  const { t } = useTranslation()
+
+  const basics = [
+    { label: t("create-modal.name"), placeholder: "Geoff Jones", key: "name" },
+    { label: t("create-modal.high-concept"), placeholder: "Noir Style hard punches and hard liquor detective", key: "aspects.highConcept" },
+    { label: t("create-modal.trouble"), placeholder: "I can't run from my past life but I can watch him trying to catch me", key: "aspects.trouble" },
+    { label: t("create-modal.relationship"), placeholder: "Knows all the cool car shops", key: "aspects.relationship" },
+    { label: t("create-modal.other-aspects"), placeholder: "Liquor and revenge keep me going", key: "aspects.otherAspect" },
+    { label: t("create-modal.other-aspects"), placeholder: "I won't die without revealing the truth", key: "aspects.secondOtherAspect" },
+  ]
+
+  const stunts = [
+    { label: "Stunt 1", key: "stunts.0" },
+    { label: "Stunt 2", key: "stunts.1" },
+    { label: "Stunt 3", key: "stunts.2" },
+    { label: "Stunt 4", key: "stunts.3" },
+    { label: "Stunt 5", key: "stunts.4" },
+    { label: "Stunt 6", key: "stunts.5" },
+  ]
+
+  const stressBoxes = [
+    { label: t("create-modal.physical-stress"), key: "maxPhysicalStress" },
+    { label: t("create-modal.mental-stress"), key: "maxMentalStress" },
+  ]
 
   const form = useForm<Character>({
     mode: "uncontrolled",
@@ -86,31 +89,31 @@ function CreateCharacterModal(props: createCharacterModalProps) {
       },
       aspects: { highConcept: "", trouble: "", relationship: "", otherAspect: "", secondOtherAspect: "" },
       skills: [
-        { name: "Academics", bonus: 0 },
-        { name: "Athletics", bonus: 0 },
-        { name: "Burglary", bonus: 0 },
-        { name: "Contacts", bonus: 0 },
-        { name: "Crafts", bonus: 0 },
-        { name: "Deceive", bonus: 0 },
-        { name: "Drive", bonus: 0 },
-        { name: "Empathy", bonus: 0 },
-        { name: "Fight", bonus: 0 },
-        { name: "Investigate", bonus: 0 },
-        { name: "Lore", bonus: 0 },
-        { name: "Notice", bonus: 0 },
-        { name: "Physique", bonus: 0 },
-        { name: "Provoke", bonus: 0 },
-        { name: "Rapport", bonus: 0 },
-        { name: "Resources", bonus: 0 },
-        { name: "Shoot", bonus: 0 },
-        { name: "Stealth", bonus: 0 },
-        { name: "Will", bonus: 0 },
+        { name: t("create-modal.skill-names.academics"), bonus: 0 },
+        { name: t("create-modal.skill-names.athletics"), bonus: 0 },
+        { name: t("create-modal.skill-names.burglary"), bonus: 0 },
+        { name: t("create-modal.skill-names.contacts"), bonus: 0 },
+        { name: t("create-modal.skill-names.crafts"), bonus: 0 },
+        { name: t("create-modal.skill-names.deceive"), bonus: 0 },
+        { name: t("create-modal.skill-names.drive"), bonus: 0 },
+        { name: t("create-modal.skill-names.empathy"), bonus: 0 },
+        { name: t("create-modal.skill-names.fight"), bonus: 0 },
+        { name: t("create-modal.skill-names.investigate"), bonus: 0 },
+        { name: t("create-modal.skill-names.lore"), bonus: 0 },
+        { name: t("create-modal.skill-names.notice"), bonus: 0 },
+        { name: t("create-modal.skill-names.physique"), bonus: 0 },
+        { name: t("create-modal.skill-names.provoke"), bonus: 0 },
+        { name: t("create-modal.skill-names.rapport"), bonus: 0 },
+        { name: t("create-modal.skill-names.resources"), bonus: 0 },
+        { name: t("create-modal.skill-names.shoot"), bonus: 0 },
+        { name: t("create-modal.skill-names.stealth"), bonus: 0 },
+        { name: t("create-modal.skill-names.will"), bonus: 0 },
       ],
       stunts: [],
       theme: "indigo",
       notes: [],
     },
-    validate: { name: (value) => (value.length === 0 ? "Your character must have a name" : null) },
+    validate: { name: (value) => (value.length === 0 ? t("create-modal.validation-error") : null) },
   })
 
   form.watch("refresh", ({ value }) => {
@@ -188,7 +191,7 @@ function CreateCharacterModal(props: createCharacterModalProps) {
   return (
     <form onSubmit={form.onSubmit(handleSubmit, handleError)}>
       <Stepper active={active} onStepClick={setActive} size="xs" mt="xs">
-        <Stepper.Step label="First Step" description="Determine basics">
+        <Stepper.Step label={t("create-modal.first-step")} description={t("create-modal.first-step-desc")}>
           <Stack>
             {basics.map((b) =>
               b.key === "name" ? (
@@ -206,7 +209,7 @@ function CreateCharacterModal(props: createCharacterModalProps) {
                   </Grid.Col>
                   <Grid.Col span={{ base: 12, sm: 12, lg: 6 }}>
                     <Stack gap={2}>
-                      <Text fw={500}> Theme</Text>
+                      <Text fw={500}>{t("create-modal.theme")}</Text>
                       <Popover>
                         <Popover.Target>
                           <Button fullWidth size="md" radius="md">
@@ -214,7 +217,7 @@ function CreateCharacterModal(props: createCharacterModalProps) {
                           </Button>
                         </Popover.Target>
                         <Popover.Dropdown>
-                          <Text style={{ textAlign: "center" }}>Theme Colors</Text>
+                          <Text style={{ textAlign: "center" }}>{t("create-modal.theme-colors")}</Text>
                           <Divider />
                           <SimpleGrid cols={4} pt="xs">
                             {colors.map((color) => (
@@ -252,7 +255,7 @@ function CreateCharacterModal(props: createCharacterModalProps) {
             )}
           </Stack>
         </Stepper.Step>
-        <Stepper.Step label="Second Step" description="Create Your stunts">
+        <Stepper.Step label={t("create-modal.second-step")} description={t("create-modal.second-step-desc")}>
           <Stack gap="sm">
             <NumberInput
               allowNegative={false}
@@ -263,14 +266,14 @@ function CreateCharacterModal(props: createCharacterModalProps) {
               key={form.key("refresh")}
               {...form.getInputProps("refresh")}
             />
-            {isMobile && <Text size="xs">You can create '6 - refresh' amount of stunts</Text>}
+            {isMobile && <Text size="xs">{t("create-modal.stunt-info")}</Text>}
             <SimpleGrid mt="sm" cols={{ base: 1, sm: 1, lg: 2 }}>
               {stunts.map((s, index) =>
                 index < creatableStuntCount ? (
                   <Paper key={index} radius="md" h={120} p="md" withBorder>
                     <Textarea
                       label={s.label}
-                      placeholder="Write your stunt"
+                      placeholder={t("create-modal.stunt-placeholder")}
                       variant="unstyled"
                       key={form.key(s.key)}
                       {...form.getInputProps(s.key)}
@@ -278,8 +281,8 @@ function CreateCharacterModal(props: createCharacterModalProps) {
                   </Paper>
                 ) : (
                   <Paper opacity={0.3} key={index} radius="md" h={120} p="md" withBorder>
-                    <Tooltip label="You can create '6 - refresh' amount of stunts">
-                      <Textarea disabled label={s.label} placeholder="Write your stunt" variant="unstyled"></Textarea>
+                    <Tooltip label={t("create-modal.stunt-info")}>
+                      <Textarea disabled label={s.label} variant="unstyled"></Textarea>
                     </Tooltip>
                   </Paper>
                 ),
@@ -287,15 +290,15 @@ function CreateCharacterModal(props: createCharacterModalProps) {
             </SimpleGrid>
           </Stack>
         </Stepper.Step>
-        <Stepper.Step label="Third Step" description="Determine your skills">
+        <Stepper.Step label={t("create-modal.third-step")} description={t("create-modal.third-step-desc")}>
           <Stack mt="sm" gap="sm">
             <Alert variant="light" icon={<IconInfoCircle />}>
-              You can select one +4, two +3, three +2, four +1 skills
+              {t("create-modal.skills-info")}
             </Alert>
             <Card withBorder shadow="sm" radius="md">
               <Card.Section withBorder inheritPadding py="xs">
                 <Group justify="space-between">
-                  <Text fw={500}>Skills</Text>
+                  <Text fw={500}>{t("create-modal.skills")}</Text>
                 </Group>
               </Card.Section>
 
@@ -317,7 +320,7 @@ function CreateCharacterModal(props: createCharacterModalProps) {
             </Card>
             {isMobile && (
               <Alert mt="md" variant="light" icon={<IconInfoCircle />}>
-                Stress boxes will calculated automaticly if you don't want to customize leave it unchecked
+                {t("create-modal.stress-info")}
               </Alert>
             )}
 
@@ -344,7 +347,7 @@ function CreateCharacterModal(props: createCharacterModalProps) {
                         {...form.getInputProps(s.key)}
                       />
                     ) : (
-                      <Tooltip label="They will calculated automaticly if you don't want to customize leave it unchecked">
+                      <Tooltip label={t("create-modal.stress-info")}>
                         <NumberInput disabled size="md" radius="md" label={s.label} />
                       </Tooltip>
                     )}
@@ -357,9 +360,9 @@ function CreateCharacterModal(props: createCharacterModalProps) {
         <Stepper.Completed>
           <Container size="xs" my="lg" p="xl">
             <Stack justify="center">
-              <Text size="xl">Thanks for using Flakter for creating your character. I hope you will like it.</Text>
+              <Text size="xl">{t("create-modal.thanks")}</Text>
               <Button size="lg" type="submit" variant="gradient" gradient={{ from: "grape", to: "cyan", deg: 90 }}>
-                Create
+                {t("create-modal.create-button")}
               </Button>
             </Stack>
           </Container>
@@ -368,9 +371,9 @@ function CreateCharacterModal(props: createCharacterModalProps) {
       {active !== 3 && (
         <Group justify="center" mt="xl">
           <Button variant="default" onClick={prevStep}>
-            Back
+            {t("create-modal.back-button")}
           </Button>
-          <Button onClick={nextStep}>Next Step</Button>
+          <Button onClick={nextStep}>{t("create-modal.next-button")}</Button>
         </Group>
       )}
     </form>
